97af6665a1242146a9bde0fbf02c21cc0efc12f0
boundlesswu <boundlesswu@gmail.com>
2017-08-10 18:16:31 +0800

    com.ceros.jectd.api

--- microservice/src/main/java/com/vorxsoft/ieye/microservice/MicroService.java
+++ microservice/src/main/java/com/vorxsoft/ieye/microservice/MicroService.java
@@ -3,15 +3,23 @@ package com.vorxsoft.ieye.microservice;
 import java.util.concurrent.ExecutionException;
 
 public interface MicroService {
+  enum PoliceType{
+    ROUND, RANDOM,PERFORMANCE;
+  }
   public void init(String endpoint) throws Exception;
   public MicroServiceImpl getInstance();
   public long LeaseGrant(int ttl) throws Exception;
   public int Registe(String name,String host,int port) throws Exception;
   public long Registe(String name,String host,int port,int ttl) throws Exception;
-  public int RegisteWithValue(String name,String host,int port,String value) throws ExecutionException, InterruptedException;
+  public int Registe(String name, String host, int port, String value) throws Exception;
+  public long Registe(String name,String host,int port,int ttl,String value) throws Exception;
+  public long RegisteWithHB(String name,String host,int port,int ttl,String value) throws Exception;
+  public long RegisteWithHB(String name,String host,int port,int ttl) throws Exception;
   public int UnRegiste(String name,String host,int port) throws Exception;
-  public String Resolve(String name);
-  public String ResolveWithWatcher(String name);
+  public String Resolve(String name) throws Exception;
+  public String Resolve(String name,PoliceType policy) throws Exception;
+  public String SetWatcher(String name)throws Exception;;
+  public String SetWatcher(String name,PoliceType policy)throws Exception;;
   public void SetMame(String name);
   public void SetHost(String host);
   public void SetPort(int port);
--- microservice/src/main/java/com/vorxsoft/ieye/microservice/MicroServiceImpl.java
+++ microservice/src/main/java/com/vorxsoft/ieye/microservice/MicroServiceImpl.java
@@ -3,19 +3,22 @@ package com.vorxsoft.ieye.microservice;
 import com.coreos.jetcd.*;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.kv.DeleteResponse;
+import com.coreos.jetcd.kv.GetResponse;
 import com.coreos.jetcd.kv.PutResponse;
+import com.coreos.jetcd.options.GetOption;
 import com.coreos.jetcd.options.PutOption;
 
 import java.security.spec.ECField;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.Random;
+import java.util.concurrent.*;
+
+import static com.vorxsoft.ieye.microservice.MicroService.PoliceType.RANDOM;
 
 public class MicroServiceImpl implements  MicroService {
   private final String microServicePath = "/service";
   private final String version = "4.0.0.1";
 
+  private String address_;
   private String name_;
   private String host_;
   private int port_;
@@ -25,6 +28,7 @@ public class MicroServiceImpl implements  MicroService {
   private Watch watchClient_;
   private String endpoint_;
   private ScheduledExecutorService executor_;
+  private Random random = new Random();
 
   MicroServiceImpl(){
     name_ = "";
@@ -36,6 +40,10 @@ public class MicroServiceImpl implements  MicroService {
     watchClient_ = null;
     endpoint_ = "";
     executor_ = Executors.newScheduledThreadPool(3);
+  }
+
+  private ScheduledExecutorService getExecutor(){
+    return  executor_;
   }
 
   @Override
@@ -65,13 +73,15 @@ public class MicroServiceImpl implements  MicroService {
   }
 
   private String Convert2key(String name, String host, int port)  {
-    return microServicePath+"/"+name+"/"+ host +":"+port;
+    return GetServiceNameFullKey(name)+"/"+ host +":"+port;
   }
-
+  private String GetServiceNameFullKey(String name){
+    return microServicePath+"/"+ name;
+  }
   @Override
   public int Registe(String name, String host, int port) throws Exception{
     String value = String.valueOf(System.currentTimeMillis());
-    return RegisteWithValue(name,host,port,value);
+    return Registe(name,host,port,value);
   }
 
   @Override
@@ -81,18 +91,43 @@ public class MicroServiceImpl implements  MicroService {
   }
   @Override
   public long Registe(String name, String host, int port, int ttl) throws Exception {
+    String value = String.valueOf(System.currentTimeMillis());
+    return Registe(name,host,port,ttl,value);
+  }
+
+  @Override
+  public long Registe(String name, String host, int port, int ttl, String value) throws Exception {
     long leaseID = LeaseGrant(ttl);
-    ByteSequence key = ByteSequence.fromString(Convert2key(name,host,port));
-    ByteSequence value = ByteSequence.fromString(String.valueOf(System.currentTimeMillis()));
-    CompletableFuture<PutResponse> feature = kvClient_.put(key,value,
+    ByteSequence key = ByteSequence.fromString(Convert2key(name, host, port));
+    ByteSequence bvalue = ByteSequence.fromString(String.valueOf(value));
+    CompletableFuture<PutResponse> feature = kvClient_.put(key, bvalue,
         PutOption.newBuilder().withLeaseId(leaseID).build());
     PutResponse response = feature.get();
     System.out.println("store response is" + response);
     return leaseID;
+}
+  @Override
+  public long RegisteWithHB(String name, String host, int port, int ttl) throws Exception {
+    String value = String.valueOf(System.currentTimeMillis());
+    return RegisteWithHB(name,host,port,ttl,value);
   }
 
   @Override
-  public int RegisteWithValue(String name, String host, int port, String value) throws ExecutionException, InterruptedException {
+  public long RegisteWithHB(String name, String host, int port, int ttl, String value) throws Exception {
+    long leaseID = Registe(name,host,port,ttl,value);
+    getExecutor().scheduleAtFixedRate(()->{
+      Lease.KeepAliveListener kal = leaseClient_.keepAlive(leaseID);
+      try {
+        System.out.println("keepAlive response  " + kal.listen());
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    },1l,ttl, TimeUnit.SECONDS);
+    return leaseID;
+  }
+
+  @Override
+  public int Registe(String name, String host, int port, String value) throws Exception {
     ByteSequence key = ByteSequence.fromString(Convert2key(name,host,port));
     PutResponse response = kvClient_.put(key,ByteSequence.fromString(value)).get();
     return 0;
@@ -109,13 +144,61 @@ public class MicroServiceImpl implements  MicroService {
     return 0;
   }
   @Override
-  public String Resolve(String name) {
+  public String Resolve(String name) throws Exception {
+    PoliceType policy = RANDOM;
+    return Resolve(name,policy);
+  }
+  private int getMaxPerformane(String name){
+    ByteSequence bkey  = ByteSequence.fromString(GetServiceNameFullKey(name));
+    return 0;
+  }
+  @Override
+  public String Resolve(String name, PoliceType policy) throws Exception {
+    ByteSequence bkey  = ByteSequence.fromString(GetServiceNameFullKey(name));
+    GetOption option = GetOption.newBuilder()
+        .withSortField(GetOption.SortTarget.KEY)
+        .withSortOrder(GetOption.SortOrder.DESCEND)
+        .withPrefix(bkey)
+        .build();
+    GetResponse response =  kvClient_.get(bkey,option).get();
+    if(response.getCount() == 0)
+      return null;
+    int i = 0;
+    switch (policy){
+      case ROUND:
+        i = 0;
+        break;
+      case RANDOM:
+        i = random.nextInt(response.getKvs().size());
+        break;
+      case PERFORMANCE:
+        i = getMaxPerformane(name);
+        break;
+      default:
+        i = 0;
+    }
+    String serviceAddress =  response.getKvs().get(i).getKey().toStringUtf8();
+    int pos = serviceAddress.lastIndexOf("/");
+    if(pos >= 0 ) return serviceAddress.substring(pos);
     return null;
   }
 
   @Override
-  public String ResolveWithWatcher(String name) {
-    return null;
+  public String SetWatcher(String name) throws Exception {
+    PoliceType policy = RANDOM;
+    return SetWatcher(name,policy);
+  }
+
+  @Override
+  public String SetWatcher(String name,PoliceType policy) throws Exception {
+    address_ = Resolve(name,policy);
+    String key = microServicePath+"/"+ name +"/"+address_;
+    Watch.Watcher mywatch  =  watchClient_.watch(ByteSequence.fromString(key));
+    getExecutor().scheduleAtFixedRate(()->{
+      //WatchResponse wreply  = mywatch.listen();
+      System.out.println("watcher response  " + mywatch.listen());
+    },1l,4l, TimeUnit.SECONDS);
+    return address_;
   }
 
   @Override
